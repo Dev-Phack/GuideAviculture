f8a7c7e73f8fc15424fda87c2c565a0f990978ff
Dupleix HOUENOU <duphackhounkpe@gmail.com>
2022-10-02 16:11:30 +0100

    feat: Création de compte et notifs #3 #12

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,7 +11,17 @@ class HomeController extends AbstractController
     #[Route('/home', name: 'app_home')]
     public function index(): Response
     {
-//        return $this->render('home/user.html.twig');
-        return $this->render('home/superadmin.html.twig');
+//        if (!$this->getUser()->isVerified()){
+//            $this->addFlash('error', 'Vous devez vérifier votre adresse email avant de pouvoir vous connecter.');
+//            return $this->redirectToRoute('app_logout');
+//        }
+//        $this->addFlash('success', 'Bienvenue sur GuideAvi');
+        if ($this->isGranted('ROLE_ADMIN')) {
+            return $this->render('home/superadmin.html.twig');
+        }
+        elseif($this->isGranted('ROLE_USER')) {
+            return $this->render('home/user.html.twig');
+        }
+        return $this->redirectToRoute('app_login');
     }
 }
